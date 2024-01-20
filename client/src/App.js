9e65de591e157b6f8740c3c669b6d56aebd18395
lmfrozza <frozzalucca@gmail.com>
2024-01-19 22:46:31 -0300

    WiP - About Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+<<<<<<< Updated upstream
+=======
+import Header from './components/Header/Header';
+import About from './components/Header/about';
+>>>>>>> Stashed changes
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+<<<<<<< Updated upstream
 
+=======
+          <Header/>
+          <About/>
+>>>>>>> Stashed changes
       </div>
     );
   }
