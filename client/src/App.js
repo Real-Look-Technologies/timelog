03c1e845707684e0b49be3c11fb15c25d3d9b892
RAFAEL BARBOSA <rafaelbardsouza@gmail,com>
2024-01-24 20:48:51 -0300

    Form WIP + Form.CSS
    Form ainda sem funcionamento, em desenvolvimento

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Form from './components/Pages/Form'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Form/>
         <Footer/>
       </div>
     );
