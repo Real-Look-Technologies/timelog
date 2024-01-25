36568ed27152a81335fa2bc4733e268faaf09822
lmfrozza <frozzalucca@gmail.com>
2024-01-24 22:51:55 -0300

    About e Home Done (CSS tá feio ainda mas faz parte)

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,13 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Form from './components/Pages/Form/Form'
 import About from './components/Pages/About/about'
-
+import Home from './components/Pages/Home/home';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        <Form/>
+        <Home/>
         <Footer/>
       </div>
     );
--- client/src/components/Pages/About/about.jsx
+++ client/src/components/Pages/About/about.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './about-style.css';
-import whitebg from '../../../resources/whitebg.png'
+
 
 export default function About() {
     return(
         <main>
-            <img src={whitebg} alt='Background' className='whitebg.png'/>
             <section id="about">
                 <h1>Apresentando a você um novo conceito no ramo logístico</h1>
                 <p>
@@ -41,26 +40,7 @@ export default function About() {
                 </ul>
             </section>
 
-            <section id="services-container">
-                <h2>Serviços</h2>
-                <ul>
-                    <li>
-                        <strong>Time Log Normal:</strong> Serviço voltado para quem precisa de uma solução logística, rodo ou aéreo conforme peso e prazo necessário.
-                    </li>
-                    <li>
-                        <strong>Time Log Urgente:</strong> Serviço voltado para quem não pode esperar, para quem precisa de uma rápida solução, com toda segurança e prazo definido.
-                    </li>
-                </ul>
-            </section>
-
-            <section id="topics-container">
-                <ul>
-                    <li>Rastreamento Completo Omnichannel Porta-a-porta</li>
-                    <li>Logística Personalizada</li>
-                    <li>Soluções Completas</li>
-                    <li>Atendimento Diferenciado</li>
-                </ul>
-            </section>
+            
 
         </main>
 
