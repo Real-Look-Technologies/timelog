c8de12a478b8b6886cd95475431eb37b4c376995
RAFAEL BARBOSA <rafaelbardsouza@gmail,com>
2024-02-01 21:00:23 -0300

    Update home.jsx

--- client/src/components/Pages/Home/home.jsx
+++ client/src/components/Pages/Home/home.jsx
@@ -1,20 +1,12 @@
-import React from 'react';
-import './style.css';
-
-export default function Home() {
+import React from "react";
+import './Home.css'
+export default function Home(){
     return(
-        <main>
-            <section id='home-text'>
-                <h1>
-                    Apresentando a você um novo conceito no ramo logístico.
-                </h1>
-                <p1>
-                    <strong>
-                        Se você não quer mais desperdiçar seu tempo, chama a TIME LOG!
-                    </strong>
-                </p1>
-            </section>
-
-        </main>
-    );
+        <div className="Homepage">
+            <h1 className="HomeTitle">VOCÊ ACHA POSSÍVEL EXPORAR MATÉRIAS-PRIMAS COM OS MELHORES PREÇOS DO MERCADO?</h1>
+            <p className="HomeText">Passe a viver experiências únicas no mercado. Estamos atentos às suas exigências e preparados para resolvê-las da melhor forma possível.</p>
+            <h1 className="HomeTitle">SEU DIA ESTÁ MUITO CORRIDO?</h1>
+            <p className="HomeText">A Time Log tem as mais completas soluções em logística para lhe auxiliar neste momento tão competitivo e no seu dia-a-dia tão corrido. Tratamos a solução para o seu problema como se fosse nosso. Não temos tempo a perder!</p>
+        </div>
+    )
 }
